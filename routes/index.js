7c0c1bfe311fa77ffb78d0aaf468abdf458128b7
liudasbo <bonckus.liudas@gmail.com>
2024-02-01 11:49:16 +0200

    Build: user can now create messages and post them on board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Luigi",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Bob",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message Board!", messages: messages });
+});
+
+/* GET message form page. */
+router.get("/new", function (req, res, next) {
+  res.render("new", { title: "Enter a new Message!" });
+});
+
+// Get data from the form
+router.post("/new", function (req, res, next) {
+  const userName = req.body.userName;
+  const messageText = req.body.messageText;
+
+  messages.push({ text: messageText, user: userName, added: new Date() });
+
+  res.redirect("/");
 });
 
 module.exports = router;
